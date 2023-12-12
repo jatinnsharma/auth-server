818b5d418a7d60f2c315c0a4012600f962f838ed
jatin <jatinsharma2231@gmail.com>
2023-12-12 21:09:17 +0530

    send automated email

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -237,3 +237,40 @@ exports.changeUserRole = asyncHandler(async(req,res)=>{
     res.status(200).json({message:`User role updated to ${role}`})
 });
 
+// send Automated emails 
+exports.sendAutomatedEmails = asyncHandler(async (req,res)=>{
+    const { subject, send_to, reply_to, template, url } = req.body;
+
+    if (!subject || !send_to || !reply_to || !template) {
+      res.status(500);
+      throw new Error("Missing email parameter");
+    }
+  
+    // Get user
+    const user = await User.findOne({ email: send_to });
+  
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  
+    const sent_from = process.env.EMAIL_USER;
+    const name = user.name;
+    const link = `${process.env.FRONTEND_URL}${url}`;
+  
+    try {
+      await sendEmail(
+        subject,
+        send_to,
+        sent_from,
+        reply_to,
+        template,
+        name,
+        link
+      );
+      res.status(200).json({ message: "Email Sent" });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Email not sent, please try again");
+    }
+})
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const {registerUser, loginUser, logoutUser, getUser, updateUser, deleteUser, getAllUsers, loginStatus, changeUserRole} = require('../controllers/user.controller');
+const {registerUser, loginUser, logoutUser, getUser, updateUser, deleteUser, getAllUsers, loginStatus, changeUserRole, sendAutomatedEmails} = require('../controllers/user.controller');
 const { protect, adminOnly, authorOnly } = require('../middlewares/auth.middleware');
 
 router.post('/register',registerUser)
@@ -13,5 +13,6 @@ router.delete('/:id',protect,adminOnly,deleteUser)
 router.get('/get-all-users',protect,authorOnly,getAllUsers)
 router.get('/login-status',loginStatus)
 router.post('/change-user-role',protect,adminOnly,changeUserRole)
+router.post('/end-automated-email',protect,sendAutomatedEmails)
 
 module.exports = router;
